e63eac859ecb75c58ddf2e6999988c438e8e441c
William Chou <wtchou712@gmail.com>
2015-02-05 10:57:59 -0600

    cleaned up code and added more award choices

--- goldenglobeWill.py
+++ goldenglobeWill.py
@@ -11,115 +11,96 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.collocations import *
 from collections import Counter
 
-
-awardsList = ['best motion picture - drama', 'best motion picture - comedy or musical', 'best director - motion picture',
-              'best mini-series or motion picture made for television', 'best screenplay - motion picture',
-              'best television series - drama', 'best television series - comedy or musical',
-              'best foreign language film', 'best animated feature film', 
-              'best actor in a motion picture, drama', 'best actor in a motion picture, musical or comedy',
-              'best actress in a motion picture, drama', 'best actress in a motion picture, musical or comedy',
-              'best supporting actor in a motion picture', 'best supporting actress in a motion picture',
-              'best actress in a tv series, drama', 'best actor in a tv series, drama',
-              'best actor in a tv series, comedy', 'best actress in a tv series, comedy',
-              'best actress in a miniseries or tv movie', 'best actor in a miniseries or tv movie',
-              'best supporting actress in a series, mini-series or tv movie', 'best supporting actor in a series, mini-series or tv movie',
-             'best original score - movie', 'best original song - motion picture'
-              ]   
-
-def remove_punct(string):
+def remove_punctuation(string):
 	string=regex.sub(' ', string)
 	return string
 
-print "================================================="
-print "\n"
-data = []
-with open('gg2013.json') as f:
-    for line in f:
-        data.append(json.loads(line))
+def removeIgnored(phrase):
+	words = phrase.split(' ')
+	stopset = set(stopwords.words('english'))
+	stopset.add('best')
+	stopset.add('golden')
+	stopset.add('globes')
+	stopset.add('goldenglobes')
+	stopset.add('goldenglobe')
+	stopset.add('rt')
+	stopset.add('actor')
+	stopset.add('actress')
+	for i in range(0,len(words)):
+		stopset.add(words[i])
+	return stopset
 
-def findRelatedTweets(award):
+def findTopTweets(phrase):
+	print "     Searching for top tweets..."
+	data = []
+	with open('gg2013.json') as f:
+	    for line in f:
+	        data.append(json.loads(line))
+
 	# this code block creates our corpus of relevant tweets - an array of tweet objects
-	relTweets = []
+	corpus = []
 	for tweet in data[0]: 
 		text = tweet["text"].lower()	
-		text = remove_punct(text)
-		#tokens = tokenize(text)
-		if "best actor in a motion picture comedy" in text:
-			relTweets.append(text)
+		text = remove_punctuation(text)
+		if phrase in text:
+			corpus.append(text)
 
 
-# this code block turns tweets into unigrams and bigrams and then stores them in token_array and bigram_array2
-token_array = []
-bigram_array = []
-bigram_array2 = []
-for x in range(0,len(corpus)):
-	#print corpus[x]
-	sentence = corpus[x]
-	tokens = nltk.word_tokenize(sentence)
-	bigrams = nltk.bigrams(sentence)
-	token_array.append(tokens)
-	#pairs = [ ", ".join(pair) for pair in nltk.bigrams(tokens)]	
-	#print bigrams
-	#bigram_array.append(pairs)
-	words = re.findall('\w+', sentence)
-	temp = zip(words,words[1:]) # originally had Counter() wrapped around zip(...)
-	bigram_array2.append(temp)
+	# this code block turns tweets into unigrams and bigrams and then stores them in token_array and bigram_array2
+	token_array = []
+	bigram_array = []
+	bigram_array2 = []
+	for x in range(0,len(corpus)):
+		#print corpus[x]
+		sentence = corpus[x]
+		tokens = nltk.word_tokenize(sentence)
+		bigrams = nltk.bigrams(sentence)
+		token_array.append(tokens)
+		words = re.findall('\w+', sentence)
+		temp = zip(words,words[1:]) # originally had Counter() wrapped around zip(...)
+		bigram_array2.append(temp)
 
-stopset = set(stopwords.words('english')) # this is the creation of our stopset -- words we don't want to include
-stopset.add('best')
-stopset.add('golden')
-stopset.add('globes')
-stopset.add('goldenglobes')
-stopset.add('goldenglobe')
-stopset.add('rt')
-stopset.add('actor')
-stopset.add('motion')
-stopset.add('picture')
-stopset.add('comedy')
-stopset.add('musical')
 
-final_array = [] # this array will store all unigrams 
-no_common_words_array = []
-#print len(token_array)
-for x in range(0,len(token_array)):
-	#print corpus[x]
-	token_list = token_array[x]
-	for y in range(0,len(token_list)):
-		one_token = token_list[y]
-		temp_array = []
-		if one_token not in stopset:
-			####print one_token	
-			final_array.append(one_token)
-			#print one_token
-			#temp_array.append(one_token)
-			#final_array.append(temp_array[0])
-			#no_common_words = (token_list)
-			#no_common_words_array.append(no_common_words)
+	stopset = removeIgnored(phrase)
 
-all_bigrams_array = [] # this array will store all bigrams
-for x in range(0,len(bigram_array2)):
-	#print corpus[x]
-	bigram_collection = bigram_array2[x]
-	#print bigram_collection
-	#print "\n"
-	for y in range(0,len(bigram_collection)):
-		one_bigram = bigram_collection[y]
-		#print one_token
-		temp_array = []
-		# ADD CODE FOR STOPSET USING IF STATEMENT BELOW... need to check a and b in tuple (a,b) for stopset match
-		if one_bigram[0] not in stopset:
-			if one_bigram[1] not in stopset:
-			####print one_token	
-				all_bigrams_array.append(one_bigram)
+	final_array = [] # this array will store all unigrams 
+	no_common_words_array = []
+	for x in range(0,len(token_array)):
+		token_list = token_array[x]
+		for y in range(0,len(token_list)):
+			one_token = token_list[y]
+			temp_array = []
+			if one_token not in stopset:
+				####print one_token	
+				final_array.append(one_token)
 
-print "UNIGRAM FREQUENCY OUTPUT"
-#print all_bigrams_array  # this is an array containing all bigrams... 
-fdist = FreqDist(final_array)
-print fdist.most_common(20)
-print "================================================="
-print "\n"
-print "BIGRAM FREQUENCY OUTPUT"
-# This is the frequency counter for bigrams... all_bigrams_array is a list of all bigrams from queried tweets 
-fdist1 = nltk.FreqDist(all_bigrams_array)
-print fdist1.most_common(20)
-print "================================================="
+	all_bigrams_array = [] # this array will store all bigrams
+	for x in range(0,len(bigram_array2)):
+		bigram_collection = bigram_array2[x]
+		for y in range(0,len(bigram_collection)):
+			one_bigram = bigram_collection[y]
+			temp_array = []
+			if one_bigram[0] not in stopset:
+				if one_bigram[1] not in stopset:
+					all_bigrams_array.append(one_bigram)
+
+	fdist = FreqDist(final_array)
+	topUni = fdist.most_common(20)
+	fdist1 = nltk.FreqDist(all_bigrams_array)
+	topBi = fdist1.most_common(20)
+
+	return topUni,topBi
+
+def findWinner(topUnigrams, topBigrams, nominees):
+	for i in range(0,len(nominees)):
+		for j in range (0,len(topUnigrams)):
+			biPart1 = (topBigrams[j][0])[0]
+			biPart2 = (topBigrams[j][0])[1]
+			uni = topUnigrams[j][0]
+			nominee = nominees[i].lower()
+			print "+++++++++++++++++++++++++++++++++++++"
+			if biPart1 or biPart2 in nominee:
+				if uni in nominee:
+					return nominees[i]
+				break
+
